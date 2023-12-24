9909a3fe85f7e8a419be6ff9c42c5039ea514b84
znc15 <jimmybandass21@gmail.com>
2023-12-24 13:22:26 +0800

    推送后端编写

--- push/cronpushemail.php
+++ push/cronpushemail.php
@@ -24,71 +24,99 @@ if (!$connection) {
     die("连接数据库失败: " . mysqli_connect_error());
 }
 
-    // 获取所有同意接收邮件的用户
-    $queryUsers = "SELECT * FROM users WHERE receive_email = 1";
-    $resultUsers = mysqli_query($connection, $queryUsers);
+// 执行 MySQL 查询
+$query = "SELECT
+            u.username,
+            u.email,
+            u.receive_email,
+            COALESCE(SUM(vc.request_count), 0) AS total_request_count
+          FROM users u
+          LEFT JOIN urls ON u.token = urls.owner
+          LEFT JOIN visitcount vc ON urls.urltoken = vc.urltoken AND vc.visit_time >= CURDATE() - INTERVAL 7 DAY
+          GROUP BY u.username, u.email, u.receive_email";
 
-    while ($rowUser = mysqli_fetch_assoc($resultUsers)) {
-        $userId = $rowUser['id'];
-        $email = $rowUser['email'];
+$result = mysqli_query($connection, $query);
 
-    // 查询该用户一周内的访问统计数据
-    $query = "SELECT COALESCE(SUM(vc.request_count), 0) as total_requests
-            FROM urls url
-            JOIN visitcount vc ON url.urltoken = vc.urltoken
-            WHERE url.owner = '$userId'
-                AND vc.visit_time >= DATE_SUB(NOW(), INTERVAL 1 WEEK)";
+// 处理查询结果
+while ($row = mysqli_fetch_assoc($result)) {
+    $username = $row['username'];
+    $email = $row['email'];
+    $receiveEmail = $row['receive_email'];
+    $totalRequestCount = $row['total_request_count'];
 
-    // 添加下面这行输出，检查 SQL 查询语句是否正确
-    echo "SQL 查询语句：{$query}<br>";
+    // 判断是否推送邮件
+    if ($receiveEmail == 1) {
+        // 使用 PHPMailer 发送邮件
+        $mail = new PHPMailer(true);
 
-    $result = mysqli_query($connection, $query);
+        try {
+            //Server settings
+            $mail->isSMTP();
+            $mail->Host       = $smtpHost;
+            $mail->SMTPAuth   = true;
+            $mail->Username   = $smtpUsername;
+            $mail->Password   = $smtpPassword;
+            $mail->SMTPSecure = $smtpProtocol;
+            $mail->Port       = $smtpPort;
 
-    // 获取总请求量
-    $totalRequests = 0;
-    if ($row = mysqli_fetch_assoc($result)) {
-        // 添加下面这行输出，检查 $row 变量的内容
-        echo "查询结果：";
-        print_r($row);
-        
-        $totalRequests = $row['total_requests'];
-    }
+            // 设置邮件内容的字符编码
+            $mail->CharSet = 'UTF-8';
 
-    // 添加下面这行输出，检查 $totalRequests 的值
-    echo "总请求量：{$totalRequests}<br>";
+            //Recipients
+            $mail->setFrom($smtpUsername, $siteName);
+            $mail->addAddress($email, $username);
 
-    // 使用 PHPMailer 发送邮件
-    $mail = new PHPMailer(true);
+            //Content
+            $mail->isHTML(true);
+            $mail->Subject = "{$siteName}-每周推送";
+            $mail->Body = "
+                <html>
+                <head>
+                    <style>
+                        body {
+                            font-family: 'Arial', sans-serif;
+                            background-color: #f4f4f4;
+                            color: #333;
+                        }
+                        .container {
+                            max-width: 600px;
+                            margin: 20px auto;
+                            padding: 20px;
+                            background-color: #fff;
+                            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                            text-align: center;
+                        }
+                        .logo {
+                            max-width: 100%;
+                            height: auto;
+                            margin-bottom: 20px;
+                        }
+                        h1 {
+                            color: #3498db;
+                        }
+                        p {
+                            line-height: 1.6;
+                        }
+                    </style>
+                </head>
+                <body>
+                    <div class='container'>
+                        <img src='{$logoimagedark}' alt='Logo' class='logo'>
+                        <h1>{$siteName}-每周推送</h1>
+                        <p>亲爱的{$username}，您本周所有网站总共被请求多少次：{$totalRequestCount}</p>
+                        <!-- 可以在这里添加更多 HTML 格式的内容 -->
+                    </div>
+                </body>
+                </html>
+            ";
 
-    // 使用 PHPMailer 发送邮件
-    $mail = new PHPMailer(true);
-
-    try {
-        //Server settings
-        $mail->isSMTP();
-        $mail->Host       = $smtpHost;
-        $mail->SMTPAuth   = true;
-        $mail->Username   = $smtpUsername;
-        $mail->Password   = $smtpPassword;
-        $mail->SMTPSecure = $smtpProtocol;
-        $mail->Port       = $smtpPort;
-
-        // 设置邮件内容的字符编码
-        $mail->CharSet = 'UTF-8';
-
-        //Recipients
-        $mail->setFrom($smtpUsername, 'Your Sender Name');
-        $mail->addAddress($email, 'Recipient Name');
-
-        //Content
-        $mail->isHTML(true);
-        $mail->Subject = 'Weekly Visit Count Summary';
-        $mail->Body    = "亲爱的用户，您本周所有网站总共被请求多少次：" . $totalRequests;
-
-        $mail->send();
-        echo '推送成功!';
-    } catch (Exception $e) {
-        echo "推送失败: {$mail->ErrorInfo}";
+            $mail->send();
+            echo "推送成功! 邮箱：{$email}<br>";
+        } catch (Exception $e) {
+            echo "推送失败: {$mail->ErrorInfo}，邮箱：{$email}<br>";
+        }
+    } else {
+        echo "用户 {$username} 不接收邮件，不推送<br>";
     }
 }
 
